af9c5eb350a907f4ef1bb4e8fade02fd614fa834
GitHub Action <action@github.com>
2024-10-29 02:51:25 +0000

    Initialize template repository proper
    Set package name to com.github.pumpkinxd.infoexport
    Set project name to infoExport
    Set modid to infoexport

--- src/main/java/com/github/pumpkinxd/infoexport/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/pumpkinxd/infoexport/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.pumpkinxd.infoexport.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
